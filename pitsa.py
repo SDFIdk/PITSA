cc206bd1c39f74e6347871b136c4e2c244bcb942
Kristian Evers <kristianevers@gmail.com>
2019-01-22 10:56:35 +0100

    Update createdb function
    Fixes several issues in the creation of timeseries and
    calculations of auxillary data. Adjusted to accept new
    file layout of input data.

--- pitsa.py
+++ pitsa.py
@@ -7,12 +7,6 @@ Interesting points:
     ALJ6EB9
     A1JDBD1
 
-
-Notes:
-
-    CREATE INDEX ts_point_codes ON ts_point (code)
-    CREATE INDEX ts_grid_codes ON ts_grid (code)
-
 """
 import math
 import re
@@ -33,6 +27,8 @@ import click
 
 import sqlite3  # fiona complains when sqlite3 is imported first
 
+ogr.UseExceptions()
+
 Param = namedtuple("Param", ["val", "stddev"])
 
 POINT_SCHEMA = {
@@ -45,6 +41,7 @@ POINT_SCHEMA = {
         "DIR": "str:1",  # Direction, ascending (A) or descending (D)
         "VEL": "float:6.1",  # Line of Sight velocity
         "V_STDEV": "float:6.1",  # LoS vel. standard deviation
+        "VEL_VERT": "float:6.1",
         "VEL_CAL": "float:6.1",  # LoS vel. calibrated with GNSS-stations
         "VEL_STDEV_CAL": "float:6.1",  # Standard deviation of calibrated LoS vel.
         "COHERENCE": "float:4.2",  # Coherence of measuring point
@@ -69,21 +66,27 @@ GRID_SCHEMA = {
         "VEL_STD_V_NOUPLIFT": "float",  # Standard deviation of the above
         "VEL_E": "float",  # Easterly velocity
         "VEL_STD_E": "float",  # Standard deviation of easterly velocity
-        "VEL_TRUNC": "float:6.1",
-        "V_R2_TRUNC": "float:6.1",
-        "VEL_CAL_TRUNC": "float:6.1",
-        "V_R2_CAL_TRUNC": "float:6.1",
-        "PERIOD": "float:6.1",
-        "PERIOD_STDEV": "float:6.1",
     },
 }
 
-TS_SCHEMA = {
+TS_LOS_SCHEMA = {
     "geometry": "None",
     "properties": {
         "CODE": "str:7",  # Point ID
         "TIME": "float",  # Decimal year of time series entry point
         "VALUE": "float",  # Value of time series
+        "CAL_VALUE": "float" # Calibrated value of time series
+    },
+}
+
+TS_2D_SCHEMA = {
+    "geometry": "None",
+    "properties": {
+        "CODE": "str:7",        # Point ID
+        "TIME": "float",        # Decimal year of time series entry point
+        "EAST": "float",        # Value of east time series
+        "VERT": "float",        # Value of vertical time series
+        "VERT_CAL": "float",    # Value of calibrated vertical time series
     },
 }
 
@@ -97,7 +100,7 @@ def sin_estimator(
     phase: float,
 ) -> np.array:
     """
-    Mathematical description of a slopeed sine function for use in curve fitting.
+    Mathematical description of a sloped sine function for use in curve fitting.
 
     Intercept and slope parameters determined by fitting a straight line and
     substituting the values using functools.partial.
@@ -151,7 +154,6 @@ def calc_stats(x: np.array, y_raw: np.array, y_cal: np.array) -> tuple:
     as well as a sinusoidal fit on the raw time series.
     """
 
-    # slope, intercept, r value
     s_raw, i_raw, r_raw, _, _ = linregress(x, y_raw)
     s_cal, _, r_cal, _, _ = linregress(x, y_cal)
     amplitude, period, phase = sin_fit(x, y_raw, i_raw, s_raw)
@@ -161,12 +163,14 @@ def calc_stats(x: np.array, y_raw: np.array, y_cal: np.array) -> tuple:
     return stats
 
 
-def add_points(basedir: Path, data_source: ogr.DataSource):
+def add_los_data(basedir: Path, data_source: ogr.DataSource, ts_source: sqlite3.Connection):
     """Add ascending/descending data and matching timeseries"""
 
-    point_lyr = data_source.GetLayerByName("point")
-    ts_raw_lyr = data_source.GetLayerByName("ts_point")
-    ts_cal_lyr = data_source.GetLayerByName("ts_point_cal")
+    point_lyr = data_source.GetLayerByName("LOS")
+    if ts_source:
+        cur = ts_source.cursor()
+        cur.execute("PRAGMA synchronous = OFF")
+        cur.execute("BEGIN TRANSACTION")
 
     for raw_shp, cal_shp in zip(basedir["raw"], basedir["cal"]):
         with fiona.open(str(raw_shp), "r") as raw, fiona.open(str(cal_shp), "r") as cal:
@@ -178,9 +182,8 @@ def add_points(basedir: Path, data_source: ogr.DataSource):
             with click.progressbar(
                 raw, label="Track: {}, Dir: {}".format(track_no, track_dir)
             ) as raw_progress:
-                for fr, fc in zip(
-                    raw_progress, cal
-                ):  # assume that raw and calibrated files are of equal lengths
+                # assume that raw and calibrated files are of equal lengths
+                for fr, fc in zip(raw_progress, cal):
                     point = ogr.CreateGeometryFromWkt(
                         "POINT({} {})".format(*fr["geometry"]["coordinates"])
                     )
@@ -193,6 +196,7 @@ def add_points(basedir: Path, data_source: ogr.DataSource):
                     feature.SetField("DIR", track_dir)
                     feature.SetField("VEL", fr["properties"]["VEL"])
                     feature.SetField("V_STDEV", fr["properties"]["V_STDEV"])
+                    feature.SetField("VEL_VERT", fr["properties"]["VEL_VERT"])
                     feature.SetField("VEL_CAL", fc["properties"]["VEL"])
                     feature.SetField("VEL_STDEV_CAL", fc["properties"]["V_STDEV"])
                     feature.SetField("COHERENCE", fr["properties"]["COHERENCE"])
@@ -200,20 +204,15 @@ def add_points(basedir: Path, data_source: ogr.DataSource):
                     feature.SetField("ADI", fr["properties"]["ADI"])
 
                     # Add statistics
-                    raw_y = np.array(
-                        [
-                            value
-                            for key, value in fr["properties"].items()
-                            if key.startswith("D")
-                        ]
-                    )
-                    cal_y = np.array(
-                        [
-                            value
-                            for key, value in fc["properties"].items()
-                            if key.startswith("D")
-                        ]
-                    )
+                    raw_y = np.array([v for k, v in fr["properties"].items() if k.startswith("D")])
+                    cal_y = np.array([v for k, v in fc["properties"].items() if k.startswith("D")])
+
+                    if ts_source:
+                        code = fr['properties']['CODE']
+                        sql = f"INSERT INTO ts_los(code, time, value, cal_value) VALUES ('{code}', ?, ?, ?)"
+                        cur.executemany(sql, zip(time_series_dates, raw_y, cal_y))
+                        ts_source.commit()
+
                     stats = calc_stats(time_series_dates[I], raw_y[I], cal_y[I])
                     feature.SetField("VEL_TRUNC", stats[0])
                     feature.SetField("V_R2_TRUNC", stats[1])
@@ -225,32 +224,31 @@ def add_points(basedir: Path, data_source: ogr.DataSource):
                     feature.SetGeometry(point)
                     feature.SetFID(point_lyr.GetFeatureCount())
                     point_lyr.CreateFeature(feature)
-                    feature.Destroy()
+                    feature.Destroy
 
-            data_source.CommitTransaction()
-            data_source.SyncToDisk()
+                data_source.CommitTransaction()
+                data_source.SyncToDisk()
 
 
-def add_grid_points(basedir: Path, data_source: ogr.DataSource):
+def add_2d_data(basedir: Path, data_source: ogr.DataSource, ts_source: sqlite3.Connection):
     """Add 2D grid data points and matching timeseries"""
-    grid_lyr = data_source.GetLayerByName("grid")
-    ts_raw_lyr = data_source.GetLayerByName("ts_grid")
-    ts_cal_lyr = data_source.GetLayerByName("ts_grid_cal")
+    grid_lyr = data_source.GetLayerByName("2D")
+    if ts_source:
+        cur = ts_source.cursor()
+        cur.execute("PRAGMA synchronous = OFF")
+        cur.execute("BEGIN TRANSACTION")
 
     east_shp = basedir["east"]
     vert_shp = basedir["vert"]
     cal_shp = basedir["cal"]
 
-    with fiona.open(str(east_shp), "r") as east, fiona.open(
-        str(vert_shp), "r"
-    ) as vert, fiona.open(str(cal_shp), "r") as cal:
+    with fiona.open(str(east_shp), "r") as east, fiona.open(str(vert_shp), "r") as vert, fiona.open(str(cal_shp), "r") as cal:
         time_series_dates = np.array(parse_dates(east.schema))
-        I = time_series_dates > 2016.083
         data_source.StartTransaction()
         with click.progressbar(east, label="Adding gridded data") as east_progress:
-            for fe, fv, fc in zip(
-                east_progress, vert, cal
-            ):  # assume that raw and calibrated files are of equal lengths
+            iterations = 0
+            # assume that raw and calibrated files are of equal lengths
+            for fe, fv, fc in zip(east_progress, vert, cal):  
                 point = ogr.CreateGeometryFromWkt(
                     "POINT({} {})".format(*fe["geometry"]["coordinates"])
                 )
@@ -263,28 +261,15 @@ def add_grid_points(basedir: Path, data_source: ogr.DataSource):
                 feature.SetField("VEL_V_NOUPLIFT", fc["properties"]["VEL_V"])
                 feature.SetField("VEL_STD_V_NOUPLIFT", fc["properties"]["V_STD_V"])
 
-                # Add statistics
-                raw_y = np.array(
-                    [
-                        value
-                        for key, value in fe["properties"].items()
-                        if key.startswith("D")
-                    ]
-                )
-                cal_y = np.array(
-                    [
-                        value
-                        for key, value in fc["properties"].items()
-                        if key.startswith("D")
-                    ]
-                )
-                stats = calc_stats(time_series_dates[I], raw_y[I], cal_y[I])
-                feature.SetField("VEL_TRUNC", stats[0])
-                feature.SetField("V_R2_TRUNC", stats[1])
-                feature.SetField("VEL_CAL_TRUNC", stats[2])
-                feature.SetField("V_R2_CAL_TRUNC", stats[3])
-                feature.SetField("PERIOD", stats[4])
-                feature.SetField("PERIOD_STDEV", stats[5])
+                east_y = np.array([value for key, value in fe["properties"].items() if key.startswith("D")])
+                vert_y = np.array([value for key, value in fv["properties"].items() if key.startswith("D")])
+                cal_y = np.array([value for key, value in fc["properties"].items() if key.startswith("D")])
+
+                if ts_source:
+                    code = fv['properties']['CODE']
+                    sql = f"INSERT INTO ts_2d(code, time, east, vert, vert_cal) VALUES ('{code}', ?, ?, ?, ?)"
+                    cur.executemany(sql, zip(time_series_dates, east_y, vert_y, cal_y))
+                    ts_source.commit()
 
                 feature.SetGeometry(point)
                 feature.SetFID(grid_lyr.GetFeatureCount())
@@ -412,71 +397,69 @@ def plot(ctx, calibrated, database, code):
 
 @pitsa.command()
 @click.argument("basedir", type=click.Path(exists=True))
-@click.argument("geopackage", type=click.Path(exists=False))
+@click.argument("data", type=click.Path(exists=False))
+@click.option("--ts", type=click.Path(exists=False), help='Path to Geopackage where times series are stored')
 @click.pass_context
-def createdb(ctx, basedir, geopackage):
+def createdb(ctx, basedir, data, ts):
     """Create a Geopackage database from the base directory of a delivery from TRE"""
 
     # Create layers in empty geopackage file (which is just a souped up sqlite database).
     # Data will be added mostly by GDAL insertions (fiona is horribly slow when adding
-    # entries to database.
-    with fiona.open(
-        geopackage,
-        "w",
-        crs=from_epsg(4326),
-        driver="GPKG",
-        layer="point",
-        schema=POINT_SCHEMA,
-    ) as gpkg:
+    # entries to database, at least pre version 1.8)
+
+    with fiona.open(data, "w", crs=from_epsg(4326), driver="GPKG", layer="LOS", schema=POINT_SCHEMA) as gpkg:
         pass
 
-    with fiona.open(
-        geopackage,
-        "w",
-        crs=from_epsg(4326),
-        driver="GPKG",
-        layer="grid",
-        schema=GRID_SCHEMA,
-    ) as gpkg:
+    with fiona.open(data, "w", crs=from_epsg(4326), driver="GPKG", layer="2D", schema=GRID_SCHEMA) as gpkg:
         pass
+
+    if ts:
+        with fiona.open(ts, 'w',  driver="GPKG", layer='TS_2D', schema=TS_2D_SCHEMA) as gpkg:
+            pass # initialize time series database
+
+        with fiona.open(ts, 'w', driver="GPKG", layer='TS_LOS', schema=TS_LOS_SCHEMA) as gpkg:
+            pass # initialize time series database
 
     dirs = {}
-    for directory in ("ASCENDING", "DESCENDING"):
-        base = Path(basedir) / Path(directory)
-        dirs[directory] = {"cal": [], "raw": []}
+    directory = 'LOS'
+    base = Path(basedir) / Path(directory)
+    dirs[directory] = {"cal": [], "raw": []}
 
-        for raw_shp in base.iterdir():
-            if raw_shp.suffix == ".shp":
+    for raw_shp in base.iterdir():
+        if raw_shp.suffix == ".shp":
+            if '_CAL_GNSS' not in raw_shp.stem:
                 dirs[directory]["raw"].append(raw_shp)
-                cal_shp = (
-                    raw_shp.parents[0]
-                    / Path("CALIBRATED")
-                    / Path(raw_shp.stem + "_CAL.shp")
-                )
+                cal_shp = (raw_shp.parents[0] / Path(raw_shp.stem + "_CAL_GNSS.shp") )
                 dirs[directory]["cal"].append(cal_shp)
+
 
     # prepare list of directories and files
     base = Path(basedir) / Path("2D")
-    dirs["2D"] = {}
-    for shp in base.iterdir():
-        if shp.suffix == ".shp":
-            if shp.stem[-1] == "V":
-                dirs["2D"]["vert"] = shp
-                calib_dir = shp.parent / Path("CALIBRATED")
-                for cal_shp in calib_dir.iterdir():
-                    if cal_shp.suffix == ".shp":
-                        dirs["2D"]["cal"] = cal_shp
-            if shp.stem[-1] == "E":
-                dirs["2D"]["east"] = shp
+    dirs["2D"] = {
+        'east': base / Path('DENMARK_SNT_EAST_IT902B3E.shp'),
+        'vert': base / Path('DENMARK_SNT_VERT_IT902B1V.shp'),
+        'cal':  base / Path('DENMARK_SNT_VERT_CAL_UPLIFT_IT902B2V.shp'),
+    }
 
-    data_source = ogr.Open(geopackage, 1)
+    print(dirs['2D'])
+    data_source = ogr.Open(data, 1)
+    ts_source = None
+    if ts:
+        ts_source = sqlite3.connect(ts)
 
-    click.secho("Adding ASCENDING data", fg="green")
-    add_points(dirs["ASCENDING"], data_source)
-    click.secho("Adding DESCENDING data", fg="green")
-    add_points(dirs["DESCENDING"], data_source)
-    click.secho("Adding GRID data", fg="green")
-    add_grid_points(dirs["2D"], data_source)
+    click.secho("Adding Line of Sight data", fg="green")
+    add_los_data(dirs["LOS"], data_source, ts_source)
+    click.secho("Adding 2D data", fg="green")
+    add_2d_data(dirs["2D"], data_source, ts_source)
+
+    if ts:
+        con = sqlite3.connect(ts)
+        cur = con.cursor()
+        click.secho("Building index for LOS timeseries")
+        cur.execute("CREATE INDEX ts_los_codes ON ts_los (code)")
+        click.secho("Building index for 2D timeseries")
+        cur.execute("CREATE INDEX ts_2d_codes ON ts_2d (code)")
+        con.commit()
 
 
 if __name__ == "__main__":
